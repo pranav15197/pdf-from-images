7acdeab49a5cbf23a454fc393bc1664be583500a
pranav <pranav.tiwari.eros@gmail.com>
2020-05-25 22:36:55 +0530

    added code to create pdf files

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import img2pdf
+
+
+def get_images(folder):
+    return [os.path.join(folder, im) for im in os.listdir(folder)]
+
+
+def create_pdf(pdfname, img_folder):
+    images = get_images(img_folder)
+    with open(pdfname, 'wb+') as f:
+        f.write(img2pdf.convert(images))
+
+if __name__ == "__main__":
+    create_pdf('final.pdf', 'images')
